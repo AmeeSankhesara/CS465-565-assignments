d4c6e0bf4790afa8af31b3decbc4b8c8bb061a4a
Ravi Shah <amisankhesara@gmail.com>
2019-11-21 12:15:03 -0800

    Added File

---
+++ Assignment-6/my-angular-app/src/main.ts
@@ -0,0 +1,6 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { ApplicationRef, enableProdMode, NgModuleRef } from '@angular/core';
+  
+platformBrowserDynamic().bootstrapModule(AppModule);
+  
